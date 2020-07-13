b892acfe4df2e103ab5f233715ee3f09472882b1
Danilo Oliveira <danilotecoliveira@gmail.com>
2020-07-13 11:46:09 -0300

    Implementando os logs e o health check

--- src/HealthCheckSites/GetHealthCheck.cs
+++ src/HealthCheckSites/GetHealthCheck.cs
@@ -10,27 +10,35 @@ namespace HealthCheckSites
 {
     public class GetHealthCheck : BackgroundService
     {
-        private readonly ILogger _logger;
+        private readonly ILogger<GetHealthCheck> _logger;
         private readonly IConfiguration _config;
         private readonly HttpClient _httpClient;
         private readonly List<string> _sites;
 
-        public GetHealthCheck(IConfiguration config, ILogger logger)
+        public GetHealthCheck(IConfiguration config, ILogger<GetHealthCheck> logger)
         {
             _logger = logger;
             _config = config;
             _httpClient = new HttpClient();
-            
+
             _sites = _config.GetSection("HealthChecks").Get<List<string>>();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            foreach (var site in _sites)
+            while (!stoppingToken.IsCancellationRequested)
             {
-                var result = await _httpClient.GetStringAsync(site);
-                
-                _logger.LogInformation(result);
+                foreach (var site in _sites)
+                {
+                    var result = await _httpClient.GetAsync(site);
+
+                    if (result.IsSuccessStatusCode)
+                        _logger.LogInformation($"Health check to {site} Ok");
+                    else
+                        _logger.LogError($"Health check error to {site}");
+                }
+
+                await Task.Delay(60000, stoppingToken);
             }
         }
     }
